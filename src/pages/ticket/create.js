9f3d8377032e48a7b298b0df9085206e740ccd36
fmtdevops <devops@fatmactech.com>
2022-11-19 21:23:36 +0530

    push data

--- src/pages/ticket/create.js
+++ src/pages/ticket/create.js
@@ -1,5 +1,5 @@
 import React from "react"; 
-import Box from '@mui/material/Box';
+import {Grid} from '@mui/material';
  
 export default class TicketListComponent extends React.Component{
 
@@ -13,8 +13,29 @@ export default class TicketListComponent extends React.Component{
     } 
 
     render(){
-        return <Box>
-          
-        </Box>
+        return  <Grid container className='fullWidth fullHeight'>
+                    <Grid item xs={12}  className='fullWidth fullHeight'>
+                            <Grid item xs={12} style={{height:'100px', background:'red'}} className='fullWidth'>
+
+                            </Grid>
+                            <Grid item xs={12} style={{height:'calc(100% - 100px)', background:'blue'}} className='fullWidth'>
+                                <Grid item xs={8} className='fullHeight'>
+                                    <Grid item xs={12} style={{height:'calc(100% - 300px)', background:'blue'}} className='fullWidth'>
+
+                                    </Grid>
+                                    <Grid item xs={12} style={{height:'200px', background:'gray'}} className='fullWidth'>
+
+                                    </Grid>
+
+                                    <Grid item xs={12} style={{height:'100px', background:'green'}} className='fullWidth'>
+
+                                    </Grid>
+                                </Grid>
+                                <Grid item xs={4} className='fullHeight'>
+
+                                </Grid>
+                            </Grid>
+                    </Grid>
+            </Grid> 
     }
 }
